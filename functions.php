a836c9a641d9b2d096f07ccbd9c8aab1a8f382f1
Benno Bielmeier <github@bbenno.com>
2021-05-24 18:32:04 +0200

    Explain the GP_PREMIUM_VERSION definition

--- functions.php
+++ functions.php
@@ -6,7 +6,12 @@
  * Only edit this file if you have direct access to it on your server (to fix errors if they happen).
  */
 
+/**
+ * Disable attention-seeking advertising banner for the premium version of GeneratePress.
+ * GeneratePress is the parent theme of this theme.
+ */
 define('GP_PREMIUM_VERSION', false);
+
 function generate_fonts_customize_register() {
   return;
 }
