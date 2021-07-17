80e9b55c7ba35e9a3109240741e7b2fec614cd37
Benno Bielmeier <github@bbenno.com>
2021-07-18 01:29:12 +0200

    PHP: Add doc comments to misc.php and esc_url

--- inc/misc.php
+++ inc/misc.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Functions concerning miscellaneous.
+ *
+ * @package rotaract-family
+ */
 
 /**
  * Set logo for backend login
@@ -12,17 +17,18 @@ function custom_login_logo() {
 			width: unset;
 		}
 		</style>',
-		get_stylesheet_directory_uri() . '/assets/img/hosting_logo.svg'
+		esc_url( get_stylesheet_directory_uri() . '/assets/img/hosting_logo.svg' )
 	);
 }
-
 add_action( 'login_head', 'custom_login_logo' );
 
 /**
  * Remove IP address on comments
+ *
+ * @param string $comment_author_ip IP address of comment author.
+ * @return string empty string
  */
 function wpb_remove_commentsip( $comment_author_ip ) {
 	return '';
 }
-
 add_filter( 'pre_comment_user_ip', 'wpb_remove_commentsip' );
