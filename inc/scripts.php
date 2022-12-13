4b7ce96872c5520f17138952074424df1600fdb6
Niklas Zbick <nicko.zbicko@gmail.com>
2022-12-13 11:36:05 +0100

    Enqueue scripts in wp action method to prevent warnings

---
+++ inc/scripts.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Register Javascript
+ */
+function enqueue_child_scripts() {
+    if ( get_theme_mod( 'let_it_snow', true ) ) {
+        wp_enqueue_script( 'snowflakes', get_stylesheet_directory_uri() . '/assets/js/snowflakes.min.js', array(), '6.0.0', true );
+    }
+    wp_enqueue_script( 'script', get_stylesheet_directory_uri() . '/assets/js/script.js', array(), '1.0.0', true );
+}
+
+add_action( 'wp_enqueue_scripts', 'enqueue_child_scripts', 60 );
