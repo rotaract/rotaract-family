15fc104509831bc3426465129a7f6c4706b1e07f
Niklas Zbick <nicko.zbicko@gmail.com>
2025-09-25 11:51:27 +0200

    Correct color defaults

--- inc/defaults.php
+++ inc/defaults.php
@@ -19,88 +19,120 @@ function child_new_defaults( $options ) {
 	$options['logo_layout']             = 'club';
 	$options['org_type']                = 'rac';
 	$options['logo_text_line_1']        = get_bloginfo( 'title' );
-	$options['background_color']        = '#f4f4f4';
+	$options['background_color']        = 'var(--base-2)';
 	$options['link_color']              = 'var(--rotaract)';
 	$options['social_media_head_color'] = 'var(--rotaract)';
-	$options['global_colors']           = array_merge(
-		$options['global_colors'],
+	$options['global_colors']           = array(
 		array(
-			array(
-				'name' => 'Cranberry',
-				'slug' => 'rotaract',
-				'color' => '#d41367',
-			),
-			array(
-				'name' => 'Azure',
-				'slug' => 'azure',
-				'color' => '#0067c8',
-			),
-			array(
-				'name' => 'Royal Blue',
-				'slug' => 'royal-blue',
-				'color' => '#17458f',
-			),
-			array(
-				'name' => 'Sky Blue',
-				'slug' => 'sky-blue',
-				'color' => '#00a2e0',
-			),
-			array(
-				'name' => 'Gold',
-				'slug' => 'gold',
-				'color' => '#f7a81b',
-			),
-			array(
-				'name' => 'Cardinal',
-				'slug' => 'cardinal',
-				'color' => '#e02927',
-			),
-			array(
-				'name' => 'Turquoise',
-				'slug' => 'turquoise',
-				'color' => '#00adbb',
-			),
-			array(
-				'name' => 'Violet',
-				'slug' => 'violet',
-				'color' => '#901f93',
-			),
-			array(
-				'name' => 'Grass',
-				'slug' => 'grass',
-				'color' => '#009739',
-			),
-			array(
-				'name' => 'Orange',
-				'slug' => 'orange',
-				'color' => '#ff7600',
-			),
-			array(
-				'name' => 'Lavender',
-				'slug' => 'lavender',
-				'color' => '#c6bcd0',
-			),
-			array(
-				'name' => 'Powder Blue',
-				'slug' => 'powder-blue',
-				'color' => '#b9d9eb',
-			),
-			array(
-				'name' => 'Slate',
-				'slug' => 'slate',
-				'color' => '#657f99',
-			),
-			array(
-				'name' => 'Charcoal',
-				'slug' => 'charcoal',
-				'color' => '#54565a',
-			),
-			array(
-				'name' => 'Mist',
-				'slug' => 'mist',
-				'color' => '#9ba4b4',
-			),
-		)
+			'name' => __( 'Contrast', 'generatepress' ),
+			'slug' => 'contrast',
+			'color' => '#212529',
+		),
+		array(
+			'name' => sprintf( __( 'Contrast %s', 'generatepress' ), '2' ),
+			'slug' => 'contrast-2',
+			'color' => '#54565a',
+		),
+		array(
+			'name' => sprintf( __( 'Contrast %s', 'generatepress' ), '3' ),
+			'slug' => 'contrast-3',
+			'color' => '#9ba4b4',
+		),
+		array(
+			'name' => __( 'Base', 'generatepress' ),
+			'slug' => 'base',
+			'color' => '#f4f4f4',
+		),
+		array(
+			'name' => sprintf( __( 'Base %s', 'generatepress' ), '2' ),
+			'slug' => 'base-2',
+			'color' => '#fafafa',
+		),
+		array(
+			'name' => sprintf( __( 'Base %s', 'generatepress' ), '3' ),
+			'slug' => 'base-3',
+			'color' => '#ffffff',
+		),
+		array(
+			'name' => sprintf( __( 'Accent', 'generatepress' ), '3' ),
+			'slug' => 'accent',
+			'color' => '#d41367',
+		),
+		array(
+			'name' => 'Cranberry',
+			'slug' => 'rotaract',
+			'color' => '#d41367',
+		),
+		array(
+			'name' => 'Azure',
+			'slug' => 'azure',
+			'color' => '#0067c8',
+		),
+		array(
+			'name' => 'Royal Blue',
+			'slug' => 'royal-blue',
+			'color' => '#17458f',
+		),
+		array(
+			'name' => 'Sky Blue',
+			'slug' => 'sky-blue',
+			'color' => '#00a2e0',
+		),
+		array(
+			'name' => 'Gold',
+			'slug' => 'gold',
+			'color' => '#f7a81b',
+		),
+		array(
+			'name' => 'Cardinal',
+			'slug' => 'cardinal',
+			'color' => '#e02927',
+		),
+		array(
+			'name' => 'Turquoise',
+			'slug' => 'turquoise',
+			'color' => '#00adbb',
+		),
+		array(
+			'name' => 'Violet',
+			'slug' => 'violet',
+			'color' => '#901f93',
+		),
+		array(
+			'name' => 'Grass',
+			'slug' => 'grass',
+			'color' => '#009739',
+		),
+		array(
+			'name' => 'Orange',
+			'slug' => 'orange',
+			'color' => '#ff7600',
+		),
+		array(
+			'name' => 'Lavender',
+			'slug' => 'lavender',
+			'color' => '#c6bcd0',
+		),
+		array(
+			'name' => 'Powder Blue',
+			'slug' => 'powder-blue',
+			'color' => '#b9d9eb',
+		),
+		array(
+			'name' => 'Slate',
+			'slug' => 'slate',
+			'color' => '#657f99',
+		),
+		array(
+			'name' => 'Charcoal',
+			'slug' => 'charcoal',
+			'color' => '#54565a',
+		),
+		array(
+			'name' => 'Mist',
+			'slug' => 'mist',
+			'color' => '#9ba4b4',
+		),
 	);
 
 	return $options;
